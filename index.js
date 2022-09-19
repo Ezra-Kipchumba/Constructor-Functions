62084a18c1a6cc59650955f7bc105de88cf122f3
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-09-19 22:25:15 +0300

    learn constructor functions

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function isPerson(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = dob;
-    this.getBirthYear = () => this.dob.getFullYear();
+    this.getBirthYear = () => this.dob;
+    this.getFullName = () => `${this.firstName} ${this.lastName}`;
 }
 
 
@@ -14,5 +15,8 @@ function isPerson(firstName, lastName, dob) {
 const person = new isPerson("Ezra", "Kipchumba", "16-01-2002");
 const person2 = new isPerson("Noela", "Kaku", "23-04-2003");
 
-console.log(person.lastName);
-console.log(person2.firstName);
+console.log(person.getBirthYear());
+console.log(person2.getFullName());
+
+// To be continued on static and class constructors 
+
